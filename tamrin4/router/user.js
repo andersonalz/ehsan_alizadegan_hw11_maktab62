c9e46d37b537a15ed192ab46c07eaeb5eb5bce98
andersonalz <86914349+andersonalz@users.noreply.github.com>
2021-12-11 21:43:39 +0330

    Update user.js

--- tamrin4/router/user.js
+++ tamrin4/router/user.js
@@ -4,15 +4,17 @@ var bodyParser = require('body-parser')
 const path = require ('path')
 const users = require (path.join(__dirname , './users.json'))
 
-user.post('/getUser' , function(req, res) {
-   let username = req.body.username;
-   
-        if (username ==='username') {
-               res.send('ok')
-        } else { 
-               res.send('not match username')
-        }
-   res.sendFile(path.join(__dirname , './users.json'))
+// user.get('/getAllUsers', function (req, res) {
+//        res.send(users)
+// })
+
+user.post('/getUser',  (req, res) => {
+       const finduser = users.find((user) => { user.username === req.bady.username })
+       if (finduser === undefined) {
+             res.send('not found') 
+       } else {
+              res.send(finduser)
+       }
 })
 
 
